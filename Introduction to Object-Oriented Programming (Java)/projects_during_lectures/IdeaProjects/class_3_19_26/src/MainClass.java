3b2f896d8f78f2f93a994a2cf637942d33701228
Haitian Wang <wong.haitian@gmail.com>
2026-03-19 14:19:36 +0800

    vault backup: 2026-03-19 14:19:36

--- Introduction to Object-Oriented Programming (Java)/projects_during_lectures/IdeaProjects/class_3_19_26/src/MainClass.java
+++ Introduction to Object-Oriented Programming (Java)/projects_during_lectures/IdeaProjects/class_3_19_26/src/MainClass.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main1(String[] args) {
         JFrame frame = new JFrame("网格布局管理器");//新建JFrame窗体
         frame.setSize(300,100);//设置窗体宽高
         JPanel panel = new JPanel();//新建JPanel面板
@@ -32,6 +32,71 @@ public class MainClass {
         panel3.add(button5, "card3");
         panel3.add(button6, "card3");
         layout.show(panel,"card3");//显示卡片2
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//设置窗体关闭响应方式
+        frame.setVisible(true);//显示窗体
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("网格包布局管理器");//新建JFrame窗体
+        frame.setSize(400,200);//设置窗体宽高
+        JPanel panel = new JPanel();//新建JPanel面板
+        frame.setContentPane(panel);//将panel设为窗体的内容面板
+
+        GridBagLayout layout = new GridBagLayout();//新建网格包布局管理器
+        panel.setLayout(layout);//设置panel的布局管理器为layout
+        GridBagConstraints constraints = new GridBagConstraints();//新建约束条件
+        constraints.fill = GridBagConstraints.BOTH;//组件填满网格
+
+        JButton button1 = new JButton("按钮---1");//新建按钮1
+        JButton button2 = new JButton("按钮---2");//新建按钮2
+        JButton button3 = new JButton("按钮---3");//新建按钮3
+        JButton button4 = new JButton("按钮---4");//新建按钮4
+        JButton button5 = new JButton("按钮---5");//新建按钮5
+        JButton button6 = new JButton("按钮---6");//新建按钮6
+        JButton button7 = new JButton("按钮---7");//新建按钮7
+
+        constraints.gridx = 0;//设置组件添加位置列数
+        constraints.gridy = 0;//设置组件添加位置行数
+        constraints.gridwidth = 2;//设置组件在网格占用的列数
+        constraints.gridheight = 2;//设置组件在网格中占用的行数
+        panel.add(button1,constraints);//向panel中添加按钮1
+
+        constraints.gridx = 2;//设置组件添加位置列数
+        constraints.gridy = 0;//设置组件添加位置行数
+        constraints.gridwidth = 2;//设置组件在网格占用的列数
+        constraints.gridheight = 1;//设置组件在网格中占用的行数
+        panel.add(button2,constraints);//向panel中添加按钮2
+
+        constraints.gridx = 2;//设置组件添加位置列数
+        constraints.gridy = 1;//设置组件添加位置行数
+        constraints.gridwidth = 1;//设置组件在网格占用的列数
+        constraints.gridheight = 1;//设置组件在网格中占用的行数
+        panel.add(button3,constraints);//向panel中添加按钮3
+
+        constraints.gridx = 3;//设置组件添加位置列数
+        constraints.gridy = 1;//设置组件添加位置行数
+        constraints.gridwidth = 1;//设置组件在网格占用的列数
+        constraints.gridheight = 2;//设置组件在网格中占用的行数
+        panel.add(button4,constraints);//向panel中添加按钮4
+
+        constraints.gridx = 0;//设置组件添加位置列数
+        constraints.gridy = 2;//设置组件添加位置行数
+        constraints.gridwidth = 1;//设置组件在网格占用的列数
+        constraints.gridheight = 1;//设置组件在网格中占用的行数
+        panel.add(button5,constraints);//向panel中添加按钮5
+
+        constraints.gridx = 1;//设置组件添加位置列数
+        constraints.gridy = 2;//设置组件添加位置行数
+        constraints.gridwidth = 1;//设置组件在网格占用的列数
+        constraints.gridheight = 1;//设置组件在网格中占用的行数
+        panel.add(button6,constraints);//向panel中添加按钮6
+
+        constraints.gridx = 2;//设置组件添加位置列数
+        constraints.gridy = 2;//设置组件添加位置行数
+        constraints.gridwidth = 1;//设置组件在网格占用的列数
+        constraints.gridheight = 1;//设置组件在网格中占用的行数
+        panel.add(button7,constraints);//向panel中添加按钮7
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//设置窗体关闭响应方式
         frame.setVisible(true);//显示窗体
