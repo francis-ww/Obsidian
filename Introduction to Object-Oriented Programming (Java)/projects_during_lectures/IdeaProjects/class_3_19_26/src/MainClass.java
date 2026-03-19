8955214049b89e36e5273492d9e72dc99cc20b69
Haitian Wang <wong.haitian@gmail.com>
2026-03-19 14:07:27 +0800

    vault backup: 2026-03-19 14:07:27

---
+++ Introduction to Object-Oriented Programming (Java)/projects_during_lectures/IdeaProjects/class_3_19_26/src/MainClass.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainClass {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("网格布局管理器");//新建JFrame窗体
+        frame.setSize(300,100);//设置窗体宽高
+        JPanel panel = new JPanel();//新建JPanel面板
+        frame.setContentPane(panel);//将panel设为窗体的内容面板
+        CardLayout layout = new CardLayout(20,10);//新建一个卡片布局管理器
+        panel.setLayout(layout);//设置layout设置为panel的布局管理器
+
+        JPanel panel1 = new JPanel("1");
+        JPanel panel2 = new JPanel("2");
+        JPanel panel3 = new JPanel("3");
+
+        panel.add(panel1);
+        
+
+        JButton button1 = new JButton("按钮---1");//新建按钮1
+        JButton button2 = new JButton("按钮---2");//新建按钮2
+        JButton button3 = new JButton("按钮---3");//新建按钮3
+
+        panel.add(button1,"card1");//向panel中添加按钮1
+        panel.add(button2,"card2");//向panel中添加按钮2
+        panel.add(button3,"card3");//向panel中添加按钮3
+        layout.show(panel,"card3");//显示卡片2
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//设置窗体关闭响应方式
+        frame.setVisible(true);//显示窗体
+    }
+
+}
